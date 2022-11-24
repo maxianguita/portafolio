539bb9873c127eb0a9eb894bc1e99971e5ef23b4
CIFU1199 <edwkamilo@gmail.com>
2022-11-24 14:52:13 -0500

    About Component Finalizado

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@
 import './App.css'
 import Header from './components/Header/Header'
 import Home from './components/home/Home'
+import About from './components/About/About'
 
 const App = () => {
   return (
@@ -10,6 +11,8 @@ const App = () => {
       
       <main className='main'>
         <Home/>
+        <About/>
+
       </main>
     </div>
   )
---
+++ src/components/About/Info.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const Info = () => {
+  return (
+    <div className="about__info grid">
+        <div className="about__box">
+            <i class='bx bx-award about__icon'></i>
+            <h3 className="about__title">Experiencia</h3>
+            <span className="about__subtitle">1 año</span>
+        </div>
+
+        <div className="about__box ">
+            <i class='bx bx-briefcase-alt about__icon' ></i>
+            <h3 className="about__title">Terminado</h3>
+            <span className="about__subtitle">3 Proyectos</span>
+        </div>
+
+        <div className="about__box ">
+            <i class='bx bx-support about__icon' ></i>
+            <h3 className="about__title">Soporte</h3>
+            <span className="about__subtitle">Online 24/7</span>
+        </div>
+    </div>
+  )
+}
+
+export default Info
