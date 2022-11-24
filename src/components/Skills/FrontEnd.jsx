3235c347ef4a41b12d4c7719513cc74879779e69
CIFU1199 <edwkamilo@gmail.com>
2022-11-24 16:21:13 -0500

    Skills Component Final

---
+++ src/components/Skills/FrontEnd.jsx
@@ -0,0 +1,71 @@
+import React from 'react'
+
+const FrontEnd = () => {
+  return (
+    <div className="skills__content">
+        <h3 className="skills__title">Desarrollo FrontEnd</h3>
+        
+        <div className="skills__box">
+            <div className="skills__group">
+                <div className="skills__data">
+                    <i class='bx bxl-html5 icon' ></i>
+                    <div>
+                        <h3 className="skills__name">HTML</h3>
+                        <span className="skills__level">Medio</span>
+                    </div>
+                </div>
+
+                <div className="skills__data">
+                    <i class='bx bxl-css3 icon' ></i>
+                    <div>
+                        <h3 className="skills__name">CSS</h3>
+                        <span className="skills__level">Alto</span>
+                    </div>
+                </div>
+                
+                <div className="skills__data">
+                    <i class='bx bxl-javascript icon' ></i>
+                    <div>
+                        <h3 className="skills__name">JavaScript</h3>
+                        <span className="skills__level">Alto</span>
+                    </div>
+                </div>
+
+                
+            </div>
+
+            <div className="skills__group">
+                <div className="skills__data">
+                    <i class='bx bxs-dashboard icon' ></i>
+                    <div>
+                        <h3 className="skills__name">Material UI</h3>
+                        <span className="skills__level">Medio</span>
+                    </div>
+                </div>
+
+                <div className="skills__data">
+                    <i class='bx bxl-github icon' ></i>
+                    <div>
+                        <h3 className="skills__name">Git | GitHub</h3>
+                        <span className="skills__level">Alto</span>
+                    </div>
+                </div>
+                
+                <div className="skills__data">
+                    <i class='bx bxl-react icon' ></i>
+                    <div>
+                        <h3 className="skills__name">React.JS</h3>
+                        <span className="skills__level">Alto</span>
+                    </div>
+                </div>
+
+                
+            </div>
+        </div>
+
+
+    </div>
+  )
+}
+
+export default FrontEnd
