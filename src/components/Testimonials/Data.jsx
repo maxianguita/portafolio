f2041c0f098c8756bd629cc2a733152e2ef7a990
CIFU1199 <edwkamilo@gmail.com>
2022-11-25 11:57:35 -0500

    Testimonial card component finalizado

--- src/components/Testimonials/Data.jsx
+++ src/components/Testimonials/Data.jsx
@@ -1,12 +1,28 @@
-import React from 'react'
 import Image1 from '../../assets/testimonial1.png'
-import Image2 from '../../assets/testimonial2.jpg'
-import Image3 from '../../assets/testimonial3.jpg'
+import Image2 from '../../assets/testimonial2.jpeg'
+import Image3 from '../../assets/testimonial3.jpeg'
 
-const Data = () => {
-  return (
-    <div>Data</div>
-  )
-}
-
-export default Data
+export const Data = [
+    {
+      id: 1,
+      image: Image1,
+      title: "Edwin Paez",
+      description:
+        "Edward es una persona dedicada con sus cosas, siempre mantiene aprendiendo y estudiando sobre nuevas tecnologías en especial de la parte web, en lo que hemos trabajado juntos siempre se ha destacado por su responsabilidad y talento.",
+    },
+    {
+      id: 2,
+      image: Image2,
+      title: "maximiliano Lujan ",
+      description:
+        "Un muy buen trabajo como compañero, todos los aspectos del proyecto fueron seguidos paso a paso y con buenos resultados.",
+    },
+    {
+      id: 3,
+      image: Image3,
+      title: "Emanuel Suares",
+      description:
+        "Comprometido con el trabajo,  destacado por su responsabilidad al momento del desarrollo de sus actividades y buen liderazgo en momento críticos.",
+    },
+  ];
+  
