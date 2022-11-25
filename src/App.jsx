6974e72d94041b737feb29bf40f1802bb29bd12a
CIFU1199 <edwkamilo@gmail.com>
2022-11-25 09:38:18 -0500

    Qualification Component Finish

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Home from './components/home/Home'
 import About from './components/About/About'
 import Skills from './components/Skills/Skills'
 import Services from './components/ServicesCard/Services'
+import Qualification from './components/Qualification/Qualification'
 
 const App = () => {
   return (
@@ -16,6 +17,7 @@ const App = () => {
         <About/>
         <Skills/>
         <Services/>
+        <Qualification/>
       </main>
     </div>
   )
