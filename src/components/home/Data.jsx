a5f426acad421066cda2ef72265038ace32e647e
maxianguita <maxi.7anguita@gmail.com>
2024-07-19 19:45:41 -0300

    cambios

--- src/components/home/Data.jsx
+++ src/components/home/Data.jsx
@@ -56,9 +56,13 @@ const Data = () => {
       </h1>
 
       <h3 className="home__subtitle">Desarrollador Front-End</h3>
-      <p className="home__description">
-     Full stack developer  –  desarrollandor de aplicaciones con React.js.
-      </p>
+<p className="home__description">
+    Full Stack Developer con especialización en desarrollo de aplicaciones utilizando React.js. 
+    Amplia experiencia en la creación de interfaces de usuario intuitivas y responsivas. 
+    Habilidades en tecnologías como JavaScript, Next.js, Node.js y MongoDB. 
+    Comprometido con el aprendizaje continuo y la implementación de mejores prácticas de desarrollo. 
+    Pasión por crear soluciones efectivas y optimizadas que mejoren la experiencia del usuario.
+</p>
 
       <a href="#contact" className="button button-flex">
         Di Hola
